a4bcec000c2cfad65de7efe5e89dc940f58c908c
Yogesh Koli <yogeshkoli@hotmail.com>
2023-02-03 18:28:04 +0530

    Fixed linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,10 @@
 """
 Sample tests
 """
-from django.test import SimpleTestCase 
+from django.test import SimpleTestCase
 
-from app import calc 
+from app import calc
+
 
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
