33ac067a5656f82bf8ae1b31f97e6e8f36e07fd0
Yogesh Koli <yogeshkoli@hotmail.com>
2023-02-05 09:59:24 +0530

    feat: subtract numbers

--- app/app/tests.py
+++ app/app/tests.py
@@ -14,3 +14,9 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        """Test Substracting Numbers"""
+        res = calc.substract(10, 15)
+
+        self.assertEqual(res, 5)
